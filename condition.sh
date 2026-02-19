3f71bfa37afbe8c1888614ca02c4aa7186ec8ab3
Chandana <rebbachandana54@gmail.com>
2026-02-19 13:51:06 +0000

    s

--- condition.sh
+++ condition.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+# NUMBER=$1
+# if [ $NUMBER -lt 10 ]; then 
+#     echo "Given number $NUMBER is less than 10"
+# elif [ $NUMBER -eq 10 ]; then
+#     echo "Given number $NUMBER is equal than 10"
+# else
+#      echo "Given number is greater than 10"
+# fi
+
 NUMBER=$1
-if [ $NUMBER -lt 10 ]; then 
-    echo "Given number $NUMBER is less than 10"
-elif [ $NUMBER -eq 10 ]; then
-    echo "Given number $NUMBER is equal than 10"
+if [ $(($NUMBER % 2)) -eq 0 ]; then 
+    echo "Given number $NUMBER is even number"
 else
-     echo "Given number is greater than 10"
+    echo "Given number $NUMBER is odd number"
 fi
