0089d34cf91b78c3e7d108f63fa6f43fd13b22fd
Chandana <rebbachandana54@gmail.com>
2026-02-19 14:02:49 +0000

    s

--- installation.sh
+++ installation.sh
@@ -4,15 +4,17 @@ if [ $USER_ID -ne 0 ]; then
     echo "script is failed please run it with root access"
     exit 1
 fi
+
 dnf install mysql-server -y 
 if [ $? -ne 0 ]; then
     echo "Mysql installation got failed..."
 else 
     echo "Mysql installation completed successfully"
+fi
 
 dnf install python -y 
 if [ $? -ne 0 ]; then
     echo "python installation got failed..."
 else 
     echo "python installation completed successfully"
-
+fi 
